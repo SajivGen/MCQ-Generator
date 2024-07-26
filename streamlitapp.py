3961aaaa9576755c828dd7263015309b42b7079c
SajivGen <sajivsomanoffl@gmail.com>
2024-07-26 11:40:31 +0530

    updated

--- streamlitapp.py
+++ streamlitapp.py
@@ -8,3 +8,80 @@ import streamlit as st
 from langchain.callbacks import get_openai_callback
 from src.mcqgenerator.MCQgenerator import generate_evaluate_chain
 from src.mcqgenerator.logger import logging
+
+#loading json file
+with open('/config/workspace/Response.json','r') as file:
+    RESPONSE_JSON = json.load(file)
+
+#Creating tile for the app
+st.title('MCQ generator using openai and langchain')    
+
+#Creating a form using st.form
+with st.form('user_inputs'):
+    #file upload
+    uploaded_file = st.file_uploader("Upload a PDF or txt file")
+
+    #input fields
+    mcq_count = st.number_input('No.of MCQs', min_value=3, max_value=50)
+
+    #subject
+    subject=st.text_input('Insert Subject', max_chars=20)
+
+    #quiz tone
+    tone = st.text_input('Complexity level of questions', max_chars=20, placeholder='Simple')
+
+    #add_button
+    button = st.form_submit_button('Create MCQs')
+
+    #check if the button is clicked and all fields have input
+
+    if file and uploaded_file is not None and mcq_count and subject and tone:
+        with st.spinner('loading.....'):
+            try:
+                text=read_file(uploaded_file)
+                #count tokens and cost of all API calls
+                with get_openai_callback() as cb:
+                    response=generate_evaluate_chain(
+                        {
+                            'text': text,
+                            'number' : mcq_count,
+                            'subject' : subject,
+                            'tone' : tone,
+                            'response_json' : json.dumps(RESPONSE_JSON)
+                        }
+                    )
+
+
+            except Exception as e:
+                traceback.print_exception(type(e), e, e.__tracback__)
+                st.error("Error")
+
+            else:
+                print(f"Total Tokens:{cb.total_tokens}")
+                print(f"Prompt Tokens:{cb.prompt_tokens}")
+                print(f"Completion Tokens:{cb.completion_tokens}")
+                print(f"Total Cost:{cb.total_cost}")   
+                if isinstance(response, dict):
+
+                #Extract quiz from the response
+                    quiz = response.get('quiz',None)
+                    if quiz is not None:
+                        table_data = get_table_data(quiz)
+                        if table_data is not None:
+                            df=pd.DataFrame(table_data)
+                            df.index=df.index+1
+                            st.table(df)
+                        
+                            #Display the review in a text box aswell
+
+                            st.text_area(label='Review', value=response['review'])
+                        else:
+                            st.error('Error in the table data')
+
+                else:
+                    st.write(response)            
+
+
+
+
+                    
